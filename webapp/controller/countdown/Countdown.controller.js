0f193f61ff3f50e865e070c20277033a0d3ddaa3
praveen m <praveengouranga@gmail.com>
2025-10-09 12:12:06 +0000

    project one tomer completed

--- webapp/controller/countdown/Countdown.controller.js
+++ webapp/controller/countdown/Countdown.controller.js
@@ -6,13 +6,35 @@ sap.ui.define([
 
     return Controller.extend("project1.controller.countdown.Countdown", {
         onInit: function () {
-            let myTileModel = new JSONModel("../model/tiles.json");
-            this.getView().setModel(myTileModel, "tiles")
+            this.timer = {
+                "days":0,
+                "hours":0,
+                "minutes":0,
+                "seconds":0
+
+            }
+            let myTileModel = new JSONModel (this.timer);
+            this.getView().setModel(myTileModel, "timer")
+            setInterval(this.calculateTime.bind(this),1000); 
+            // this.calculateTime()
         },
-        onPress: function (oEvent) {
-            alert(oEvent);
+
+        calculateTime:function(){
+          let techedDate = new Date("Dec 8 2026");
+          let currentDate = new Date();
+          let diff = techedDate.getTime() - currentDate.getTime();
+          this.timer.days = Math.floor(diff / ( 1000 * 60 * 60 * 24 ));
+          this.timer.hours = Math.floor((diff % ( 1000 * 60 * 60 * 24 )) / ( 1000 * 60 * 60 ));
+          this.timer.minutes = Math.floor((diff % ( 1000 * 60 * 60 ))/ ( 1000 * 60  ) );
+          this.timer.seconds = Math.floor((diff % ( 1000 * 60  ))/ ( 1000  ) );
+          this.getView().getModel("timer").setData(this.timer);
+
+
+
+
 
 
         }
+
     });
 });
