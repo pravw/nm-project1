e6176f0b26940d16cb38800fcea7f37840b4a219
praveen m <praveengouranga@gmail.com>
2025-10-23 12:52:04 +0000

    external navigation

--- webapp/controller/Tiles.controller.js
+++ webapp/controller/Tiles.controller.js
@@ -11,20 +11,28 @@ sap.ui.define([
         },
 
 
-        // press: function (oEvent) {
-        //      this.getOwnerComponent().getRouter().navTo("first")
-        //      this.getOwnerComponent().getRouter().navTo("second")
-            
-
-
-        // }
-
+        
 
 
         handleTilePress: function (oEvent) {
-    const oSource = oEvent.getSource();
-    const oBindingContext = oSource.getBindingContext("tiles");
-    const sRoute = oBindingContext.getProperty("route");
+
+             const oSource = oEvent.getSource();
+    
+              // Step 2: get the binding context of that source
+             const oBindingContext = oSource.getBindingContext("tiles");
+    
+              // Step 3: get the route from the binding context
+               const sRoute = oBindingContext.getProperty("route");
+           
+            if (sRoute.substring(0,4)  == 'EXT-'){
+
+         let selItem=  JSON.parse(this.getView().getModel("tiles").getJSON()).find(item => {if (item.route == sRoute){ return item;}} );
+         sap.m.URLHelper.redirect(selItem.url);
+            }
+    // const oSource = oEvent.getSource();
+    // const oBindingContext = oSource.getBindingContext("tiles");
+    //  const sRoute = oBindingContext.getProperty("route");
+    
     
     this.getOwnerComponent().getRouter().navTo(sRoute);
 }
